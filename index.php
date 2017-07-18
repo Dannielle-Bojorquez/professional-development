0072fb6f59e9cb0c245761a9bc537aa8a2f80ff9
Dannielle Bojorquez <dbojorquez@cnm.edu>
2017-07-18 10:00:41 -0600

    adding body

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Professional Development</title>
+	</head>
+	<body>
+		<h1>Dannielle Bojorquez Professional Development</h1>
+		<h2>Quantitative</h2>
+		<ul>
+			<li>10 plus years in customer service experiance</li>
+			<li>5 years in a hospital setting</li>
+			<li>I also have supervisory experiance in resturants</li>
+		</ul>
+		<h2>Qualitative</h2>
+		<p> </p>
+	</body>
+</html>
