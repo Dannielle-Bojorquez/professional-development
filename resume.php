03736ce3e079623911a8e4beb8c32838f2e586c3
Dannielle Bojorquez <dbojorquez@cnm.edu>
2017-07-31 20:03:46 -0600

    added team comment to pro dev doc, edited resume

---
+++ resume.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Resume</title>
+
+	</head>
+	<body>
+		<h1>Dannielle Bojorquez</h1>
+		<h3>Albuquerque, NM (87105)</h3>
+		<h3>(dannielle.bojorquez@gmail.com) – (5054330504)</h3>
+		<p>I am seeking to be part of a high performing team, providing quality service to the community. I would like to enhance
+			skills that I already have while acquiring new ones. Excellent communication skills along with very solid customer
+			service knowledge. Authorized to work in the US for any employer</p>
+
+		<h2>Work Experiance</h2>
+		<h3>Er Registrar</h3>
+		<p>Presbyterian Hospital  - Albuquerque, NM – (2012) to (current)</p>
+		<ul>
+			<li>Obtain and Complete updates on patients demographic, financial, and compliance information</li>
+			<li>Calculate patient's responsibilities</li>
+			<li>Advises patient and family of co-pays, deductibles, and coinsurance for appointments</li>
+			<li>Collects patient payments and issue receipts</li>
+			<li>Ensure compliance and HIPPA, EMTALA, and JACHO</li>
+		</ul>
+
+		<h3>Insurance Verification Specialist</h3>
+		<p>Presbyterian Healthcare Services - Albuquerque, NM – (2015) to (2017)</p>
+		<ul>
+			<li>Verify Insurance</li>
+			<li>Contact Insurance companies to obtain Benefits and Eligibility</li>
+			<li>Contact Insurance to obtain Authorizations</li>
+			<li>Work a WQ and Work 60 + accounts daily</li>
+			<li>Fax information to Insurance companies regarding patient admissions to hospital</li>
+			<li>Maintaining an organized and clean working space</li>
+		</ul>
+
+		<h3>Supervisor</h3>
+		<p>Fresquez Concessions - Albuquerque, NM – (2011) To (2012)</p>
+		<ul>
+			<li>Responsible for all daily operational duties</li>
+			<li>Responsible for all daily</li>
+			<li>Maintaining quality operational duties</li>
+			<li>Ensure the proper training of employees and procedures customer service as well as product</li>
+			<li>Cash handling</li>
+			<li>Making orders</li>
+		</ul>
+		<h3>Cashier</h3>
+		<p>Borders Books - Albuquerque, NM – (2006) to (2011)</p>
+		<ul>
+			<li>Responsible for keeping care of the treatment rooms, records of the office and patients, instruments etc.</li>
+			<li>Create display on the Sales floor</li>
+			<li>Cash handling</li>
+			<li>Making recommendations</li>
+			<li>Re-shelving discarded books</li>
+			<li>Processing sales quickly, accurately, and efficiently</li>
+		</ul>
+		<h2>Education</h2>
+		<p>College - CNM - Albuquerque,NM - Present</p>
+		<p>High School - Rio Grande - Albuquerque,NM - 2006</p>
+		<h2>Skills</h2>
+		<ul>
+			<li>Enjoy working with public</li>
+			<li>Detailed and People orientated</li>
+			<li>Great team player</li>
+			<li>Quick learner</li>
+			<li>Excellent knowledge of the clerical and computer skills like word, excel, outlook, access.</li>
+			<li>Data entry and filing.</li>
+			<li>Exceptionally good demonstration skills and communication skills both verbally and written.</li>
+			<li>Reliable, Self-starter, motivated</li>
+			<li>Basic knowledge</li>
+			<li>Experience in Micros and Epic.</li>
+		</ul>
+
+		<h2>Referrals</h2>
+		<ul>
+			<li>Theresa West 505-697-0791</li>
+				<li>Vanessa Robles 505- 315-4862</li>
+				<li>Lauren Gallegos 505-217-5006</li>
+		</ul>
+	</body>
+</html>
+
+
+
+
